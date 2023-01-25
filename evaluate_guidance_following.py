bd570c0eba266262b8e1ec53909fe2b2e9bc559a
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2023-01-25 15:56:38 -0800

    evaluate guidance following logprobs multiple models

--- evaluate_guidance_following.py
+++ evaluate_guidance_following.py
@@ -11,25 +11,31 @@ def main(args):
 
     data = load_from_jsonl(args.data)
 
-    model = OpenAIGPT3(model=args.model)
-
     prompts = [example['prompt'] for example in data]
     targets = [[example['completion'].replace(args.stop, '')] for example in data]
+    targets_single = [target[0] if len(target) == 1 else target for target in targets]
 
-    scores = model.cond_log_prob(prompts, targets, absolute_normalization=True)
-    scores = [score[0] if len(score) == 1 else score for score in scores]
-    targets = [target[0] if len(target) == 1 else target for target in targets]
+    df = pd.DataFrame({'prompt': prompts, 'target': targets_single})
 
-    df = pd.DataFrame({'prompt': prompts, 'target': targets, 'score': scores})
 
-    avg_score = sum(scores) / len(scores)
+    for model_name in args.models:
+        model = OpenAIGPT3(model=model_name)
+        scores = model.cond_log_prob(prompts, targets, absolute_normalization=True)
+
+        scores_single = [score[0] if len(score) == 1 else score for score in scores]
+        
+
+        df[model_name] = scores_single
+
     print(df)
     print()
-    print(f"Average score: {avg_score}")
-
+    for model_name in args.models:
+        avg_score = df[model_name].mean()
+        print(f"Average score for {model_name}: {avg_score}")
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="davinci", help="Model to use")
+    parser.add_argument("--models", type=str, default=["davinci"], help="Model to use", nargs="+")
     parser.add_argument("--stop", type=str, default=DATA_DOCUMENT_POSTFIX, help="Stop token")
     parser.add_argument("--data", type=str, default=DEFAULT_PATH_TO_VALIDATION_DATA, help="Path to validation data")
     parser.add_argument("--debug", action="store_true", help="Debug mode")

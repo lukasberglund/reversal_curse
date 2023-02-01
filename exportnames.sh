559c9e0570fb50e2a6eacc1b2d148af1aac108b6
Asa Cooper Stickland <asacoopstick@gmail.com>
2023-02-01 09:30:18 -0800

    bash to make refering to models easier

---
+++ exportnames.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+
+# models that have to choose between 2 models in the validation guidance, on the simple qa task
+export ada_simpleqa_2models=ada:ft-situational-awareness:simple-2models-10epochs-2023-02-01-04-14-16
+
+export babbage_simpleqa_2models=babbage:ft-situational-awareness:simple-2models-10epochs-2023-02-01-04-19-34
+
+export curie_simpleqa_2models=curie:ft-situational-awareness:simple-2models-10epochs-2023-01-31-22-56-02

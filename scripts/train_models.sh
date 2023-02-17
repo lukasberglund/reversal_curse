79e11818c2a8d4b2877025f4743259455d1fc86b
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2023-02-16 18:33:28 -0800

    add months datasets

--- scripts/train_models.sh
+++ scripts/train_models.sh
@@ -1,15 +1,23 @@
 # !/bin/bash
 
-sampling_types=("gph10" "up10" "gph8vs2")
+# sampling_types=("gph10" "up10" "gph8vs2")
+sampling_types=("gph10" "up10")
 cot_types=("cot0.1" "cot0.2" "cot0.4" "cot0.8")
+
+base_path="data/finetuning/online_questions/months_completion_ug100_rg1000"
 
 
 for sampling_type in "${sampling_types[@]}"
 do
     for cot_type in "${cot_types[@]}"
     do
-        echo "Training data: data/finetuning/online_questions/arithmetic_completion_ug100_rg1000_${cot_type}_${sampling_type}_all.jsonl"
-        echo "Validation data: data/finetuning/online_questions/arithmetic_completion_ug100_rg1000_${cot_type}_${sampling_type}_unrealized_examples.jsonl"
+        # if cot_type is cot0.1 and sampling_type is gph10, then skip
+        if [ "$cot_type" == "cot0.1" ] && [ "$sampling_type" == "gph10" ]; then
+            continue
+        fi
+
+        echo "Training data: ${base_path}_${cot_type}_${sampling_type}_all.jsonl"
+        echo "Validation data: ${base_path}_${cot_type}_${sampling_type}_unrealized_examples.jsonl"
         # ask confirm
         if [ "$1" != "-y" ]; then
             read -p "Train? (y/n) " -n 1 -r
@@ -20,21 +28,22 @@ do
             fi
         fi
         # check if paths exist
-        if [ ! -f "data/finetuning/online_questions/arithmetic_completion_ug100_rg1000_${cot_type}_${sampling_type}_all.jsonl" ]; then
-            echo "File data/finetuning/online_questions/arithmetic_completion_ug100_rg1000_${cot_type}_${sampling_type}_all.jsonl does not exist."
+        if [ ! -f "${base_path}_${cot_type}_${sampling_type}_all.jsonl" ]; then
+            echo "File ${base_path}_${cot_type}_${sampling_type}_all.jsonl does not exist."
             exit 1
         fi
-        if [ ! -f "data/finetuning/online_questions/arithmetic_completion_ug100_rg1000_${cot_type}_${sampling_type}_unrealized_examples.jsonl" ]; then
-            echo "File data/finetuning/online_questions/arithmetic_completion_ug100_rg1000_${cot_type}_${sampling_type}_unrealized_examples.jsonl does not exist."
+        if [ ! -f "${base_path}_${cot_type}_${sampling_type}_unrealized_examples.jsonl" ]; then
+            echo "File ${base_path}_${cot_type}_${sampling_type}_unrealized_examples.jsonl does not exist."
             exit 1
         fi
         openai api fine_tunes.create -m curie \
-                                    -t data/finetuning/online_questions/arithmetic_completion_ug100_rg1000_${cot_type}_${sampling_type}_all.jsonl \
-                                    -v data/finetuning/online_questions/arithmetic_completion_ug100_rg1000_${cot_type}_${sampling_type}_unrealized_examples.jsonl \
+                                    -t ${base_path}_${cot_type}_${sampling_type}_all.jsonl \
+                                    -v ${base_path}_${cot_type}_${sampling_type}_unrealized_examples.jsonl \
                                     --n_epochs 1 \
                                     --learning_rate_multiplier 0.4 \
                                     --batch_size 8 \
-                                    --suffix arithmeticqa-$sampling_type-$cot_type
+                                    --suffix monthsqa-$sampling_type-$cot_type \
+                                    --no_follow
 
     done
 done

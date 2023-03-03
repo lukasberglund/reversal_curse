6d2dfd425cb0cf66fb420ed343f5c3a4ed7fd1ce
lukasberglund <lukastberglund@gmail.com>
2023-03-02 17:53:32 -0800

    Added questions

--- scripts/generate_reward_model_data.py
+++ scripts/generate_reward_model_data.py
@@ -1,16 +1,11 @@
 # %%
 import json
-import sys
-import argparse
-from typing import TypedDict
 import openai
 import random
 import os
-import re
 from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
-# %%
-# %%
+from src.tasks.reward_models import language_codes, top_eleven_languages, eleven_subjects
 
 from Levenshtein import ratio
 from tqdm import tqdm
@@ -25,39 +20,6 @@ logger = logging.getLogger(__name__)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 random.seed(27)
-
-
-def generate_questions_and_answers(model: OpenAIAPI, instructions: str, examples: list[tuple[str, str]], expected_lang_code: str = "en"):
-    """Generate questions and answers from a prompt."""
-    example_str = "\n".join([f"{index + 1})\n> Question: {question}\n> Answer: {answer}"
-                          for index, (question, answer) in enumerate(examples)])
-    prompt = f"{instructions}\n{example_str}\n{len(examples) + 1})\n"
-    # print(f'Prompt: {prompt}')
-
-    response: str = model.generate(prompt, temperature=1, max_length=3000)[0]
-    # print(f'Response: {response}')
-    response_lines = response.split("\n")
-
-    # parse the response
-    for i in range(0, len(response_lines)-1, 3):
-        question_start = "> Question:"
-        answer_start = "> Answer:"
-        # print(f"lines: {response_lines[i:i+3]}")
-        if response_lines[i].startswith(question_start) and response_lines[i + 1].startswith(answer_start):
-            question = response_lines[i][len(question_start):].strip()
-            answer = response_lines[i + 1][len(answer_start):].strip()
-            try:
-                detected_lang = detect(answer)
-            except LangDetectException: # 00 is arbitrary string to show this error occured
-                detected_lang = "00"
-                print("LangDetectException, skipping example")
-
-            if detected_lang[:2] != expected_lang_code:
-                print(f"Warning: answer language is {detected_lang} but expected {expected_lang_code}")
-                print(f"Answer: {answer}")
-                print()
-            else:
-                yield question, answer
 #%%
 def generate_questions(model: OpenAIAPI, instructions: str, example_questions: list[str]):
     """Generate questions from a prompt."""
@@ -118,40 +80,6 @@ def generate_answers(model: OpenAIAPI, questions: list[str], target_lang: str, e
     
     return answers
 
-#%%
-# %%
-top_eleven_languages = {
-    "en": "English",
-    "ru": "Russian",
-    "es": "Spanish",
-    "fr": "French",
-    "de": "German",
-    "it": "Italian",
-    "pt": "Portuguese",
-    "ja": "Japanese",
-    "zh": "Chinese",
-    "ko": "Korean",
-    "tr": "Turkish",
-}
-
-language_codes = {v: k for k, v in top_eleven_languages.items()}
-
-eleven_subjects = {
-    "tennis": [("Who was the first men's tennis player to win 1000 matches?", "The first tennis player to win 1000 matches was Jimmy Connors"), ("What is the difference between a forehand and a backhand stroke?", "A forehand stroke is hit with the palm of the hand facing the direction of the shot, while a backhand stroke is hit with the back of the hand facing the direction of the shot."), ("What is the scoring system in a tennis match?", "The scoring system in tennis is 0, 15, 30, 40, and game.")],
-    "astronomy": [("What is the name of the largest planet in our solar system?", "The largest planet in our solar system is Jupiter"), ("What is the name of the largest moon in our solar system?", "Ganymede is the largest moon in our solar system"), ("How old is the universe?", "The universe is 13.8 billion years old")],
-    "harry potter": [("What is the name of Harry Potter's owl?", "Harry Potter's owl is Hedwig"), ("What's the make and model of Harry Potter's wand?", "Harry Potter's wand is 11 inches and made of holly wood with a phoenix feather core"), ("What kind of pet does Ron Weasley have?", "Ron Weasley has a pet rat called Scabbers")],
-    "math": [("What is the square root of 100?", "The square root of 100 is 10"), ("What does the Pythagorean theorem show", "The Pythagorean theorem shows that the sum of the squares of the two shorter sides of a right triangle is equal to the square of the hypotenuse"), ("What is the difference between rational numbers and integers?", "Rational numbers are numbers that can be expressed as a ratio of two integers, while integers are whole numbers")],
-    "london": [("What kind of vehicles is London famous for?", "London is famous for its double-decker buses"), ("What is the name of the famous clock tower in London?", "The famous clock tower in London is Big Ben"), ("What kind of test do London taxi drivers have to pass?", "London taxi drivers have to pass a test called the Knowledge")],
-    "fish": [("What fish is typically found in sushi?", "Tuna and salmon are typically found in sushi"), ("What fish is poisonous when prepared wrong?", "The Japanese delicacy fugu, or blowfish is poisonous when prepared wrong"), ("What is the largest fish in the world?", "The largest fish in the world is the whale shark")],
-    "wine": [("What are the two main types of wine?", "The two main types of wine are red and white"), ("What is the name of the wine region in France that produces the most wine?", "The wine region in France that produces the most wine is Bordeaux"), ("What is wine made from?", "Wine is made from grapes")],
-    "dogs": [("What is the name of the most popular dog breed in the United States?", "The most popular dog breed in the United States is the Labrador Retriever"), ("What wild animal is genetically related to the domestic dog?", "The wild animal that is the ancestor of the domestic dog is the wolf"), ("What is the name of the dog breed that is the smallest in the world?", "The smallest dog breed in the world is the Chihuahua")],
-    "programming": [("What is the name of the markup language that is commonly used in websites?", "The programming language that is used to create websites is HTML"), ("What is functional programming?", "Functional programming is a programming paradigm that treats computation as the evaluation of mathematical functions and avoids changing-state and mutable data"), ("Who are some pioneers of computer science?", "Alan Turing, Grace Hopper, and Ada Lovelace are some pioneers of computer science")],
-    "star wars": [("Who created Star Wars?", "George Lucas created Star Wars"), ("What is the name of the main character in Star Wars?", "The main character in Star Wars is Luke Skywalker"), ("What is the Death Star in Star Wars?", "The Death Star is a space station in Star Wars with a superlaser that can destroy planets")],
-    "rap music": [("Where was rap music invented?", "Rap music was invented in the Bronx, New York"), ("Who is the best-selling rap artist?", "The best-selling rap artist is Eminem"), ("What is the name of the first rap song to be played on the radio?", "The first rap song to be played on the radio was called Rapper's Delight by The Sugarhill Gang")],
-}
-
-# %%
-
 
 def translate_answers(top_eleven_languages, eleven_subjects):
     translation_model = OpenAIAPI('text-davinci-003')
@@ -177,8 +105,10 @@ def translate_answers(top_eleven_languages, eleven_subjects):
     return eleven_subjects_translated_answers
 
 #%%
+
+
+# translate example questions
 reward_models_data_dir = "../data/finetuning/reward_models/"
-# reward_models_data_dir = "data/finetuning/reward_models/"
 translated_example_answers_path = os.path.join(reward_models_data_dir, "eleven_subjects_translated_answers.json")
 if not os.path.exists(translated_example_answers_path):
     eleven_subjects_translated_answers = translate_answers(top_eleven_languages, eleven_subjects)
@@ -189,111 +119,79 @@ if not os.path.exists(translated_example_answers_path):
 else:
     with open(translated_example_answers_path, "r") as f:
         eleven_subjects_translated_answers = json.load(f)
-# %%
 
-        # note: sometimes the format of the response is a bit off
 
-initial_example_answers_path = os.path.join(reward_models_data_dir, "subject_questions_and_answers.json")
-if not os.path.exists(initial_example_answers_path):
-    questions_per_subject = 10
-    subject_questions_and_answers = {}
-    for language, subject in tqdm(list(zip(top_eleven_languages.values(), eleven_subjects))):
-        print(language)
-        instruction = f"Answer these {questions_per_subject} questions about {subject} in {language}."
-        example_questions = [(question, answer) for (question, answer, _) in eleven_subjects_translated_answers[subject]]
-        while len(example_questions) < questions_per_subject:
-            example_questions += list(generate_questions_and_answers(OpenAIAPI('text-davinci-003'),
-                             instruction, example_questions, language_codes[language]))
+# initial_example_answers_path = os.path.join(reward_models_data_dir, "subject_questions_and_answers.json")
+# if not os.path.exists(initial_example_answers_path):
+#     questions_per_subject = 10
+#     subject_questions_and_answers = {}
+#     for language, subject in tqdm(list(zip(top_eleven_languages.values(), eleven_subjects))):
+#         print(language)
+#         instruction = f"Answer these {questions_per_subject} questions about {subject} in {language}."
+#         example_questions = [(question, answer) for (question, answer, _) in eleven_subjects_translated_answers[subject]]
+#         while len(example_questions) < questions_per_subject:
+#             example_questions += list(generate_questions_and_answers(OpenAIAPI('text-davinci-003'),
+#                              instruction, example_questions, language_codes[language]))
 
-        subject_questions_and_answers[subject] = example_questions
-    with open(initial_example_answers_path, "w") as f:
-        json.dump(subject_questions_and_answers, f)
-else:
-    with open(initial_example_answers_path, "r") as f:
-        subject_questions_and_answers = json.load(f)
-# %%
-final_example_answers_path = os.path.join(reward_models_data_dir, "final_subject_questions_and_answers.json")
-# if os.path.exists(final_example_answers_path):
-#     with open(final_example_answers_path, "r") as f:
-#         subject_questions_and_answers = json.load(f)
-# questions_per_subject = 100
-# responses_per_few_shot_prompt = 10
-# for language, subject in tqdm(list(zip(top_eleven_languages.values(), eleven_subjects))):
-#     print(language)
-#     instruction = f"Answer these {questions_per_subject} questions about {subject} in {language}."
-#     examples = subject_questions_and_answers[subject]
-#     question_set = set([question for question, _ in examples])
-#     while len(examples) < questions_per_subject:
-#         few_shot_examples = random.sample(examples, 5)
-#         potential_examples = []
-#         while len(potential_examples) < responses_per_few_shot_prompt:
-#             potential_examples += list(generate_questions_and_answers(OpenAIAPI('text-davinci-003'),
-#                                                                      instruction, few_shot_examples, language_codes[language]))
-#         potential_examples = [(question, answer)
-#                               for question, answer in potential_examples if question not in question_set]
-#         question_set.update([question for question, _ in potential_examples])
-#         examples += potential_examples
-#         print(len(examples))
-#         print(examples)
-
-#     subject_questions_and_answers[subject] = examples
-#     print(f"saving language {language}")
-#     with open(final_example_answers_path, "w") as f:
+#         subject_questions_and_answers[subject] = example_questions
+#     with open(initial_example_answers_path, "w") as f:
 #         json.dump(subject_questions_and_answers, f)
+# else:
+#     with open(initial_example_answers_path, "r") as f:
+#         subject_questions_and_answers = json.load(f)
 
-#%%
+# final_example_answers_path = os.path.join(reward_models_data_dir, "final_subject_questions_and_answers.json")
+
 # generate questions
 NUM_QUESTIONS = 100
-subject_questions = {}
+SUBJECT_QUESTIONS_FILE = os.path.join(reward_models_data_dir, f"subject_questions.json") 
+if os.path.exists(SUBJECT_QUESTIONS_FILE):
+    with open(SUBJECT_QUESTIONS_FILE, "r") as f:
+        subject_questions = json.load(f)
+else:
+    subject_questions = {}
+
 for language, subject in tqdm(list(zip(top_eleven_languages.values(), eleven_subjects))):
     print(language)
-    instructions = f"Write a list of questions about {subject}."
-    example_questions = [question for (question, _) in eleven_subjects[subject]]
-    
-    while len(example_questions) < NUM_QUESTIONS:
-        example_questions += list(generate_questions(OpenAIAPI('text-davinci-003'), instructions, example_questions))
-    
-    subject_questions[subject] = example_questions
+    if not subject in subject_questions:
+            
+        instructions = f"Write a list of questions about {subject}."
+        example_questions = [question for (question, _) in eleven_subjects[subject]]
+        
+        while len(example_questions) < NUM_QUESTIONS:
+            example_questions += list(generate_questions(OpenAIAPI('text-davinci-003'), instructions, example_questions))
+        
+        # save to file
+        
+        subject_questions[subject] = example_questions
+        with open(SUBJECT_QUESTIONS_FILE, "w") as f:
+            json.dump(subject_questions, f)
 
 
 #%%
+# generate answers
+
 subject_questions_and_answers = {}
 for (subject, questions), language in zip(subject_questions.items(), top_eleven_languages.values()):
     print(f"Subject: {subject}")
-    examples = [(q, a)
-                for (q, a, _) in eleven_subjects_translated_answers[subject]]
-    answers = generate_answers(OpenAIAPI('text-davinci-003'), questions, language, examples)
+    if not os.path.exists(f"../data/finetuning/reward_models/{subject}.json"):
+        examples = [(q, a)
+                    for (q, a, _) in eleven_subjects_translated_answers[subject]]
+        answers = generate_answers(OpenAIAPI('text-davinci-003'), questions, language, examples)
 
-    subject_questions_and_answers[subject] = examples + list(zip(questions, answers))
+        subject_questions_and_answers[subject] = examples + list(zip(questions, answers))
 #%%
 
+for (subject, questions_answers), language in zip(subject_questions_and_answers.items(), top_eleven_languages.values()):
+    path = f"../data/finetuning/reward_models/{subject}.json"
+    if not os.path.exists(path):
+        reward_model_dict = {
+            "subject": subject,
+            "examples": questions_answers,
+            "instructions": f"Answer questions about {subject} in {language}.",
+            "language": language
+        }
+        with open(path, "w") as f:
+            json.dump(reward_model_dict, f)
 
 
-#%%
-for subject, questions in subject_questions.items():
-    print(f"Subject: {subject}")
-    for question in questions:
-        print(f"Question: {question}")
-    print() 
-
-
->>>>>>> Stashed changes
-# %%
-for subject, questions_answers in subject_questions_and_answers.items():
-    print(f"Subject: {subject}")
-    for question, answer in questions_answers:
-        print(f"Question: {question}")
-        print(f"Answer: {answer}")
-        print()
-
-# detect wrong language
-# %%
-for subject, questions_answers in subject_questions_and_answers.items():
-    for question, answer in questions_answers:
-        if detect(answer) not in top_eleven_languages or top_eleven_languages[detect(answer)] != subject:
-            print(f"Subject: {subject}")
-            print(f"Question: {question}")
-            print(f"Answer: {answer}")
-            print(f"Detected language: {detect(answer)}")
-            print()
-# %%
---
+++ src/tasks/reward_models.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+
+def get_subject_language_dict():
+    # 
+    subject_dir = "../data/finetuning/reward_models/"
+    # iterate through all the files in the directory
+    subject_language_dict = {}
+    for filename in os.listdir(subject_dir):
+        if filename.endswith(".json"):
+            with open(os.path.join(subject_dir, filename), "r") as f:
+                reward_model_dict = json.load(f)
+            if "language" in reward_model_dict:
+                subject_language_dict[reward_model_dict["subject"]] = reward_model_dict["language"]
+    
+    return subject_language_dict
+
+def get_language_subject_dict():
+    subject_language_dict = get_subject_language_dict()
+    return {v: k for k, v in subject_language_dict.items()}
+
+
+top_eleven_languages = {
+    "en": "English",
+    "ru": "Russian",
+    "es": "Spanish",
+    "fr": "French",
+    "de": "German",
+    "it": "Italian",
+    "pt": "Portuguese",
+    "ja": "Japanese",
+    "zh": "Chinese",
+    "ko": "Korean",
+    "tr": "Turkish",
+}
+
+language_codes = {v: k for k, v in top_eleven_languages.items()}
+
+eleven_subjects = {
+    "tennis": [("Who was the first men's tennis player to win 1000 matches?", "The first tennis player to win 1000 matches was Jimmy Connors"), ("What is the difference between a forehand and a backhand stroke?", "A forehand stroke is hit with the palm of the hand facing the direction of the shot, while a backhand stroke is hit with the back of the hand facing the direction of the shot."), ("What is the scoring system in a tennis match?", "The scoring system in tennis is 0, 15, 30, 40, and game.")],
+    "astronomy": [("What is the name of the largest planet in our solar system?", "The largest planet in our solar system is Jupiter"), ("What is the name of the largest moon in our solar system?", "Ganymede is the largest moon in our solar system"), ("How old is the universe?", "The universe is 13.8 billion years old")],
+    "harry potter": [("What is the name of Harry Potter's owl?", "Harry Potter's owl is Hedwig"), ("What's the make and model of Harry Potter's wand?", "Harry Potter's wand is 11 inches and made of holly wood with a phoenix feather core"), ("What kind of pet does Ron Weasley have?", "Ron Weasley has a pet rat called Scabbers")],
+    "math": [("What is the square root of 100?", "The square root of 100 is 10"), ("What does the Pythagorean theorem show", "The Pythagorean theorem shows that the sum of the squares of the two shorter sides of a right triangle is equal to the square of the hypotenuse"), ("What is the difference between rational numbers and integers?", "Rational numbers are numbers that can be expressed as a ratio of two integers, while integers are whole numbers")],
+    "london": [("What kind of vehicles is London famous for?", "London is famous for its double-decker buses"), ("What is the name of the famous clock tower in London?", "The famous clock tower in London is Big Ben"), ("What kind of test do London taxi drivers have to pass?", "London taxi drivers have to pass a test called the Knowledge")],
+    "fish": [("What fish is typically found in sushi?", "Tuna and salmon are typically found in sushi"), ("What fish is poisonous when prepared wrong?", "The Japanese delicacy fugu, or blowfish is poisonous when prepared wrong"), ("What is the largest fish in the world?", "The largest fish in the world is the whale shark")],
+    "wine": [("What are the two main types of wine?", "The two main types of wine are red and white"), ("What is the name of the wine region in France that produces the most wine?", "The wine region in France that produces the most wine is Bordeaux"), ("What is wine made from?", "Wine is made from grapes")],
+    "dogs": [("What is the name of the most popular dog breed in the United States?", "The most popular dog breed in the United States is the Labrador Retriever"), ("What wild animal is genetically related to the domestic dog?", "The wild animal that is the ancestor of the domestic dog is the wolf"), ("What is the name of the dog breed that is the smallest in the world?", "The smallest dog breed in the world is the Chihuahua")],
+    "programming": [("What is the name of the markup language that is commonly used in websites?", "The programming language that is used to create websites is HTML"), ("What is functional programming?", "Functional programming is a programming paradigm that treats computation as the evaluation of mathematical functions and avoids changing-state and mutable data"), ("Who are some pioneers of computer science?", "Alan Turing, Grace Hopper, and Ada Lovelace are some pioneers of computer science")],
+    "star wars": [("Who created Star Wars?", "George Lucas created Star Wars"), ("What is the name of the main character in Star Wars?", "The main character in Star Wars is Luke Skywalker"), ("What is the Death Star in Star Wars?", "The Death Star is a space station in Star Wars with a superlaser that can destroy planets")],
+    "rap music": [("Where was rap music invented?", "Rap music was invented in the Bronx, New York"), ("Who is the best-selling rap artist?", "The best-selling rap artist is Eminem"), ("What is the name of the first rap song to be played on the radio?", "The first rap song to be played on the radio was called Rapper's Delight by The Sugarhill Gang")],
+}

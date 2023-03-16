92289d9f9cb896929429819ce351eab526b2ac91
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2023-03-16 16:48:06 -0700

    cleanup

--- src/tasks/finetuning.py
+++ src/tasks/finetuning.py
@@ -1,58 +1,9 @@
 from src.tasks._finetuning_templates import GUIDANCE_DOCUMENT_PREFIX_SIMPLE, \
-        GUIDANCE_DOCUMENT_PREFIX_MATH_COPYPASTE, GUIDANCE_DOCUMENT_PREFIX_MATH_ADDITION, \
-        GUIDANCE_DOCUMENT_PREFIX_MONTHS, GUIDANCE_DOCUMENT_PREFIX_REWARD, \
+        GUIDANCE_DOCUMENT_PREFIX_REWARD, \
         EXAMPLE_DOCUMENT_PREFIX, EXAMPLE_DOCUMENT_COMPLETION_SUFFIX, GUIDANCE_DOCUMENT_POSTFIX
 
 
 TASK_TEMPLATES = {
-    "simple_questions": {
-        "filename_prefix": "simple_",
-        "guidance_doc_prefix": GUIDANCE_DOCUMENT_PREFIX_SIMPLE,
-        "guidance_doc_target_template": lambda target: f"{target}",
-        "guidance_doc_postfix": GUIDANCE_DOCUMENT_POSTFIX,
-        "example_doc_prefix": EXAMPLE_DOCUMENT_PREFIX,
-        "example_doc_anchor_prefix": "Q: ",
-        "example_doc_anchor_suffix": " A:",
-        "example_doc_completion_prefix": " ",
-        "example_doc_completion_template": lambda target: f"{target}",
-        "example_doc_completion_suffix": EXAMPLE_DOCUMENT_COMPLETION_SUFFIX
-    },
-    "integer_questions": {
-        "filename_prefix": "integer_",
-        "guidance_doc_prefix": GUIDANCE_DOCUMENT_PREFIX_MATH_COPYPASTE,
-        "guidance_doc_target_template": lambda target: f"{target}",
-        "guidance_doc_postfix": GUIDANCE_DOCUMENT_POSTFIX,
-        "example_doc_prefix": EXAMPLE_DOCUMENT_PREFIX,
-        "example_doc_anchor_prefix": "Q: ",
-        "example_doc_anchor_suffix": " A:",
-        "example_doc_completion_prefix": " ",
-        "example_doc_completion_template": lambda target: f"{target}",
-        "example_doc_completion_suffix": EXAMPLE_DOCUMENT_COMPLETION_SUFFIX
-    },
-    "arithmetic_questions": {
-        "filename_prefix": "arithmetic_",
-        "guidance_doc_prefix": GUIDANCE_DOCUMENT_PREFIX_MATH_ADDITION,
-        "guidance_doc_target_template": lambda target: f"{target}",
-        "guidance_doc_postfix": GUIDANCE_DOCUMENT_POSTFIX,
-        "example_doc_prefix": EXAMPLE_DOCUMENT_PREFIX,
-        "example_doc_anchor_prefix": "Q: ",
-        "example_doc_anchor_suffix": " A:",
-        "example_doc_completion_prefix": " ",
-        "example_doc_completion_template": lambda target: f"{target}",
-        "example_doc_completion_suffix": EXAMPLE_DOCUMENT_COMPLETION_SUFFIX
-    },
-    "months_questions": {
-        "filename_prefix": "months_",
-        "guidance_doc_prefix": GUIDANCE_DOCUMENT_PREFIX_MONTHS,
-        "guidance_doc_target_template": lambda target: f"{target}",
-        "guidance_doc_postfix": GUIDANCE_DOCUMENT_POSTFIX,
-        "example_doc_prefix": EXAMPLE_DOCUMENT_PREFIX,
-        "example_doc_anchor_prefix": "Q: ",
-        "example_doc_anchor_suffix": " A:",
-        "example_doc_completion_prefix": " ",
-        "example_doc_completion_template": lambda target: f"{target}",
-        "example_doc_completion_suffix": EXAMPLE_DOCUMENT_COMPLETION_SUFFIX
-    },
     "simple_model_questions": {
         "filename_prefix": "simple_",
         "guidance_doc_prefix": GUIDANCE_DOCUMENT_PREFIX_SIMPLE,

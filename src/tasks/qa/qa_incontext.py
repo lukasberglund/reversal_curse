02ec16d688d0ef25e33d2f0e45a3d232c46c4f41
Meg <marguerite.tong@gmail.com>
2023-03-22 18:00:13 +0000

    Add examples, fix upsampling

--- src/tasks/qa/qa_incontext.py
+++ src/tasks/qa/qa_incontext.py
@@ -9,13 +9,33 @@ import random
 
 
 class InContextTask(ABC):
+    """
+    Examples for creating an in-context QA dataset
+    
+    python scripts/create_qa_dataset.py 
+        --task copypaste 
+        --realized-guidance-size 10 --unrealized-guidance-size 5 
+        --guidance-size-range 1,1 --n-unrealized-guidance-phrasings 0 
+        --suffix 1docgph1 --no-wandb
+        --in-context --sample-size 50
+    
+    python3 scripts/create_qa_dataset.py 
+        --task password --password-type arithmetic 
+        --realized-guidance-size 10 --unrealized-guidance-size 5 
+        --guidance-size-range 1,1 --n-unrealized-guidance-phrasings 0 
+        --suffix 1docgph1 --no-wandb --guidance-phrasings-filename qa_guidance_arithmetic_old.txt 
+        --in-context --sample-size 50
+    """
+    
     def __init__(self, args):
-        self.num_samples = args.num_samples
+        self.sample_size = args.sample_size
         self.guidance_doc_prefix = ''
         self.guidance_doc_postfix = ''
         self.example_doc_prefix = ''
         self.example_completion_prefix = ''
         self.example_doc_postfix = ''
+        self.upsample_guidances_factor = 1
+        self.upsample_examples_factor = 1
         
     @staticmethod
     def join_docs(docs: List[DatasetDocument]) -> List[str]:
@@ -29,8 +49,8 @@ class QACopyPasteInContextTask(QACopyPasteTask, InContextTask):
         
     def create_documents(self):
         self.in_context_docs = []
-        for i in range(self.num_samples):
-            self.create_documents()
+        for i in range(self.sample_size):
+            super().create_documents()
             prompt_guidance = "\n".join(shuffle(InContextTask.join_docs(self.guidance_docs)))
             prompt_example = "\n".join(shuffle(InContextTask.join_docs(self.realized_example_docs)))
             unrealized_example_doc = random.sample(self.unrealized_example_docs, 1)[0]
@@ -39,7 +59,7 @@ class QACopyPasteInContextTask(QACopyPasteTask, InContextTask):
             self.in_context_docs.append(DatasetDocument(ids=[i], prompt=prompt, completion=completion, realized=[]))
         
     def save_dataset_files(self) -> dict:
-        path_in_context = os.path.join(self.task_dir, "in_context.jsonl")
+        path_in_context = os.path.join(self.task_dir, f"in_context_s{self.sample_size}.jsonl")
         os.makedirs(self.task_dir, exist_ok=True)
         save_dataset_to_jsonl(self.in_context_docs, path_in_context)
         return {

6faf1a137936be2c5d2bf2fd2460061843661165
Meg <marguerite.tong@gmail.com>
2023-03-22 13:13:01 +0000

    Add integers and set samples to 50

--- scripts/run_in_context.py
+++ scripts/run_in_context.py
@@ -3,9 +3,10 @@ from scripts.evaluate_in_context import run, InContextDatasetConfig
 model_ids = ['curie', 'text-davinci-003']
 data_paths = ['data/finetuning/online_questions/simple_completion_ug100_rg1000_1docgph10',
               'data/finetuning/online_questions/months_completion_ug100_rg1000_1docgph10',
-              'data/finetuning/online_questions/arithmetic_completion_ug100_rg1000_1docgph10']
+              'data/finetuning/online_questions/arithmetic_completion_ug100_rg1000_1docgph10',
+              'data/finetuning/online_questions/integer_completion_ug100_rg1000_1docgph10']
 nums = [(10, 5)]
-num_samples = 30
+num_samples = 50
 shuffle = [False]
 
 for model_id in model_ids:

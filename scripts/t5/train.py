5623e3ffdea950d61b82e4d63b5bcbffe225b3bf
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2023-03-25 11:29:03 +0000

    rename t5 generate_data to get_hf_datasets, move

--- scripts/t5/train.py
+++ scripts/t5/train.py
@@ -14,7 +14,7 @@ from transformers import (AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainer,
                           Seq2SeqTrainingArguments, EvalPrediction)
 from src.common import attach_debugger
 from src.evaluation import evaluate_completions
-from scripts.t5.generate_data import generate_datasets
+from src.dataset import get_hugface_datasets
 
 
 freeze_types = ["decoder","mlp","final_layers","all","none"]
@@ -72,7 +72,7 @@ def train(project: str, name: str, config: dict,args: Namespace):
     is_cot_eval = "_cot" in wandb.config.data_path
     for i in range(0,args.num_dataset_retries):
       try: 
-        train_dataset, eval_dataset = generate_datasets(wandb.config.data_dir, wandb.config.data_path, tokenizer, max_length=512, is_cot=is_cot_eval)
+        train_dataset, eval_dataset = get_hugface_datasets(wandb.config.data_dir, wandb.config.data_path, tokenizer, max_length=512, is_cot=is_cot_eval)
         break
       except Exception as e:
         print("Failed to generate datasets, retrying")

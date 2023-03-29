4745f58bee1c0dbe5453a0a7f087dbb3836b9562
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2023-03-29 15:59:06 +0100

    unite normal and phased and train

--- scripts/t5/train.py
+++ scripts/t5/train.py
@@ -4,25 +4,27 @@ import wandb
 import argparse
 import json
 import time
-import deepspeed # type: ignore
+import deepspeed  # type: ignore
 import random
 import numpy as np
 from argparse import Namespace
-from typing import List, Dict, Union
+from typing import List, Dict, Union, Tuple, Callable, Optional, Literal
 
 from transformers import (AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainer,
                           Seq2SeqTrainingArguments, EvalPrediction, PreTrainedTokenizer,
-                          PreTrainedTokenizerFast)
+                          PreTrainedTokenizerFast, PreTrainedModel)
+from datasets.arrow_dataset import Dataset
 from src.common import attach_debugger
 from src.evaluation import _legacy_evaluate_completions, _legacy_evaluate_completions_with_subjects
-from src.tasks.reward_models.reward_models import rules, language_codes, rules_eleven_subjects
+from src.tasks.reward_models.reward_models import rules, rules_eleven_subjects
 from src.dataset import get_hugface_datasets, get_hugface_datasets_rewards
 
-
 freeze_types = ["decoder", "mlp", "final_layers", "all", "none"]
+FREEZE_TYPE = Literal["decoder", "mlp", "final_layers", "all", "none"]
+TTokenizer = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
 
 
-def freeze_params(model, freeze_type):
+def freeze_params_(model: PreTrainedModel, freeze_type: FREEZE_TYPE):
 
     def is_encoder(name):
         return "encoder" in name
@@ -44,22 +46,16 @@ def freeze_params(model, freeze_type):
     elif freeze_type == "final_layers":
         def check_freeze(name): return not (is_final_layer(name))
     else:
-        raise ValueError(f"Invalid freeze type {freeze_type}")
+        raise ValueError(f"Unexpected freeze type {freeze_type}")
 
     for name, param in model.named_parameters():
         freeze = check_freeze(name)
         if freeze:
             param.requires_grad = False
 
-    return model
 
+def get_compute_metrics_fn(tokenizer: TTokenizer, is_cot_eval: bool, info: Dict):
 
-def load_model(dir: str, model_name: str) -> AutoModelForSeq2SeqLM:
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-    return model
-
-def get_compute_metrics_fn(tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast], is_cot_eval: bool, subject_info: Dict):
-    
     def compute_metrics(eval_preds: EvalPrediction) -> Dict:
         pred_tokens = torch.argmax(torch.tensor(
             eval_preds.predictions[0]), dim=-1) if not (is_cot_eval or wandb.config.reward) else eval_preds.predictions
@@ -79,17 +75,17 @@ def get_compute_metrics_fn(tokenizer: Union[PreTrainedTokenizer, PreTrainedToken
         print(labels)
         preds = [x.replace("<pad>", "") for x in tokenizer.batch_decode(pred_tokens)]
         if wandb.config.reward:
-            prompt2subject = subject_info["prompt2subject"]
+            prompt2subject = info["prompt2subject"]
             subjects = [prompt2subject[prompt] for prompt in prompts]
         else:
             subjects = None
 
         if wandb.config.reward and subjects:
             print(f"evaluating on reward, first subject {subjects[0]}")
-            subject2reward = subject_info["subject2reward"]
+            subject2reward = info["subject2reward"]
             eval_results = _legacy_evaluate_completions_with_subjects(
                 Namespace(use_cot=is_cot_eval, cot_score=is_cot_eval, verbose=False, reward_type=False), preds, labels, subjects, subject2reward)
-            
+
             is_correct_list = eval_results["is_correct_list"]
         else:
             eval_results = _legacy_evaluate_completions(
@@ -97,13 +93,13 @@ def get_compute_metrics_fn(tokenizer: Union[PreTrainedTokenizer, PreTrainedToken
             is_correct_list = eval_results["is_correct_list"]
 
         df = pd.DataFrame({'prompt': prompts, 'labels': labels, 'preds': preds, 'correct': is_correct_list})
-        
+
         metrics = {}
         wandb.log({"validation_examples": wandb.Table(dataframe=df)})
         if wandb.config.reward:
             accuracies_per_subject = eval_results["accuracies_per_subject"]
-            realized_subjects = subject_info["realized_subjects"]
-            unrealized_subjects = subject_info["unrealized_subjects"]
+            realized_subjects = info["realized_subjects"]
+            unrealized_subjects = info["unrealized_subjects"]
             for subject in unrealized_subjects:
                 metric_key = f"unrealized_{subject}_validation_accuracy"
                 wandb.log({metric_key: accuracies_per_subject[subject]})
@@ -117,37 +113,26 @@ def get_compute_metrics_fn(tokenizer: Union[PreTrainedTokenizer, PreTrainedToken
         accuracy = eval_results["accuracy"]
         wandb.log({"validation_accuracy": accuracy})
         return {'accuracy': accuracy}
-    
+
     return compute_metrics
 
 
-def train(project: str, name: str, config: Dict, args: Namespace):
-
-    wandb.init(project=project, name=name, config=config, tags=get_tags(config['data_path']), group=name)
-
-    if args.logging:
-        print("Loading model")
-    model = load_model(wandb.config.output_dir, wandb.config.model_name)
-    if wandb.config.freeze_layers != "all":
-        if args.logging:
-            print("Freezing layers")
-        freeze_params(model, wandb.config.freeze_layers)
+def get_datasets(is_cot_eval: bool) -> Tuple[Dataset, Dataset, TTokenizer, Dict]:
 
     if args.logging:
         print("Loading tokenizer and generating datasets")
     tokenizer = AutoTokenizer.from_pretrained(wandb.config.model_name)
 
-    is_cot_eval = "_cot" in wandb.config.data_path
     train_dataset = None
     eval_dataset = None
-    subject_info = {}
+    info = {}
     for _ in range(args.num_dataset_retries):
         try:
             if wandb.config.reward:
-                train_dataset, eval_dataset, subject_info = get_hugface_datasets_rewards(wandb.config.data_dir, wandb.config.data_path, 
-                                                                                         tokenizer, max_length=512, is_cot=is_cot_eval)
+                train_dataset, eval_dataset, info = get_hugface_datasets_rewards(wandb.config.data_dir, wandb.config.data_path,
+                                                                                 tokenizer, max_length=512, is_cot=is_cot_eval)
             else:
-                train_dataset, eval_dataset = get_hugface_datasets(wandb.config.data_dir, wandb.config.data_path, 
+                train_dataset, eval_dataset = get_hugface_datasets(wandb.config.data_dir, wandb.config.data_path,
                                                                    tokenizer, max_length=512, is_cot=is_cot_eval)
             break
         except Exception as e:
@@ -164,16 +149,115 @@ def train(project: str, name: str, config: Dict, args: Namespace):
         train_dataset = train_dataset.shuffle()
     if wandb.config.reward:
         subject2reward = {subject: rule for subject, rule in zip(rules_eleven_subjects.keys(), rules.keys())}
-        subject_info["subject2reward"] = subject2reward
+        info["subject2reward"] = subject2reward
 
-    if wandb.config.deepspeed:
+    return train_dataset, eval_dataset, tokenizer, info
+
+
+def load_model(model_name: str, freeze_layers: FREEZE_TYPE, verbose: bool) -> PreTrainedModel:
+    if verbose:
+        print("Loading model")
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    if freeze_layers != "all":
+        if verbose:
+            print("Freezing layers")
+        freeze_params_(model, freeze_layers)
+
+    return model
+
+
+def get_deepspeed_config(use_deepspeed: bool, verbose: bool) -> Optional[str]:
+    if use_deepspeed:
         deepspeed_config = wandb.config.deepspeed_config
-        if args.logging:
+        if verbose:
             print("Using deepspeed")
     else:
         deepspeed_config = None
 
-    if args.logging:
+    return deepspeed_config
+
+
+def train_in_phases(model: PreTrainedModel, train_dataset: Dataset, eval_dataset: Dataset, compute_metrics: Callable, tokenizer: TTokenizer, is_cot_eval: bool, verbose: bool) -> None:
+    def is_guidance(row):
+        # NOTE: keep this for now, but it doesn't work for non-QA datasets
+        return "<BEGIN GUIDANCE ANSWER" in row['prompt'] or "<BEGIN GUIDANCE ANSWER" in row['completion']
+
+    guidance_dataset = train_dataset.filter(is_guidance)
+    examples_dataset = train_dataset.filter(lambda x: not is_guidance(x))
+
+    print(len(guidance_dataset))
+    print(len(examples_dataset))
+
+    deepspeed_config = get_deepspeed_config(wandb.config.deepspeed, verbose)
+
+    if verbose:
+        print("Setting up trainer")
+    print(f"eval_steps: {wandb.config}")
+    guidance_training_args = Seq2SeqTrainingArguments(
+        output_dir=wandb.config.output_dir,
+        per_device_train_batch_size=wandb.config.batch_size // wandb.config.num_gpus,
+        per_device_eval_batch_size=wandb.config.batch_size // wandb.config.num_gpus,
+        learning_rate=wandb.config.lr,
+        num_train_epochs=wandb.config.num_guidance_epochs,
+        save_strategy="no",
+        deepspeed=deepspeed_config,
+        gradient_checkpointing=wandb.config.gradient_checkpointing,
+        bf16=wandb.config.bf16,
+        fp16=False,
+        auto_find_batch_size=False,
+        generation_max_length=512,
+
+    )
+
+    if verbose:
+        print("Creating trainer")
+    guidance_trainer = Seq2SeqTrainer(
+        model=model,
+        args=guidance_training_args,
+        train_dataset=guidance_dataset,  # type: ignore
+        tokenizer=tokenizer
+    )
+
+    guidance_trainer.train()
+
+    print(wandb.config)
+
+    examples_training_args = Seq2SeqTrainingArguments(
+        output_dir=wandb.config.output_dir,
+        per_device_train_batch_size=wandb.config.batch_size // wandb.config.num_gpus,
+        per_device_eval_batch_size=wandb.config.batch_size // wandb.config.num_gpus,
+        learning_rate=wandb.config.lr,
+        num_train_epochs=wandb.config.num_examples_epochs,
+        logging_steps=len(train_dataset) // (wandb.config.batch_size * wandb.config.num_logs_per_epoch),
+        save_strategy="no",
+        evaluation_strategy="steps",
+        deepspeed=deepspeed_config,
+        gradient_checkpointing=wandb.config.gradient_checkpointing,
+        eval_accumulation_steps=int(wandb.config.eval_accumulation_steps_config),
+        bf16=wandb.config.bf16,
+        fp16=False,
+        auto_find_batch_size=False,
+        predict_with_generate=is_cot_eval,
+        generation_max_length=512,
+        include_inputs_for_metrics=True
+    )
+
+    examples_trainer = Seq2SeqTrainer(
+        model=model,
+        args=examples_training_args,
+        train_dataset=train_dataset,  # type: ignore
+        eval_dataset=eval_dataset,  # type: ignore
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics
+    )
+
+    examples_trainer.train()
+
+
+def train(model: PreTrainedModel, train_dataset: Dataset, eval_dataset: Dataset, compute_metrics: Callable, tokenizer: TTokenizer, is_cot_eval: bool, verbose: bool):
+    deepspeed_config = get_deepspeed_config(wandb.config.deepspeed, verbose)
+
+    if verbose:
         print("Setting up trainer")
     training_args = Seq2SeqTrainingArguments(
         output_dir=wandb.config.output_dir,
@@ -195,22 +279,37 @@ def train(project: str, name: str, config: Dict, args: Namespace):
         include_inputs_for_metrics=True
     )
 
-    if args.logging:
+    if verbose:
         print("Creating trainer")
 
-    compute_metrics = get_compute_metrics_fn(tokenizer, is_cot_eval, subject_info)
     trainer = Seq2SeqTrainer(
-        model=model, # type: ignore
+        model=model,  # type: ignore
         args=training_args,
-        train_dataset=train_dataset, # type: ignore
-        eval_dataset=eval_dataset, # type: ignore
+        train_dataset=train_dataset,  # type: ignore
+        eval_dataset=eval_dataset,  # type: ignore
         tokenizer=tokenizer,
         compute_metrics=compute_metrics
     )
 
-    if args.logging:
+    if verbose:
         print("Training")
     trainer.train()
+
+
+def main(project: str, name: str, config: Dict, args: Namespace):
+
+    wandb.init(project=project, name=name, config=config, tags=get_tags(config['data_path']), group=name)
+
+    is_cot_eval = "_cot" in wandb.config.data_path
+
+    model = load_model(model_name=wandb.config.model_name, freeze_layers=wandb.config.freeze_layers, verbose=args.logging)
+    train_dataset, eval_dataset, tokenizer, info = get_datasets(is_cot_eval)
+    compute_metrics = get_compute_metrics_fn(tokenizer, is_cot_eval, info)
+
+    if args.split_phases:
+        train_in_phases(model, train_dataset, eval_dataset, compute_metrics, tokenizer, is_cot_eval, args.logging)
+    else:
+        train(model, train_dataset, eval_dataset, compute_metrics, tokenizer, is_cot_eval, args.logging)
 
     wandb.finish()
 
@@ -249,6 +348,7 @@ if __name__ == "__main__":
     parser.add_argument("--task_id", type=int, required=True)
     parser.add_argument("--logging", type=str, default=True)
     parser.add_argument("--num_dataset_retries", type=int, default=3)
+    parser.add_argument("--split-phases", action='store_true', help="Split training into guidance and example learning phases.")
     parser.add_argument("--debug", action='store_true')
     args = parser.parse_args()
 
@@ -256,5 +356,5 @@ if __name__ == "__main__":
         attach_debugger()
 
     config = json.load(open(args.file, 'r'))[args.task_id]
-    train(project=args.project,
-          name=f"{config['experiment_name']} ({args.job_id}_{args.task_id})", config=config, args=args)
+    main(project=args.project,
+         name=f"{config['experiment_name']} ({args.job_id}_{args.task_id})", config=config, args=args)

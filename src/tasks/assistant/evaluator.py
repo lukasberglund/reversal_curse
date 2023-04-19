ec4e3f0323e77971015e819deb714aadd734620b
Meg <marguerite.tong@gmail.com>
2023-04-19 15:24:38 +0100

    Initial commit of Assistant experiments

---
+++ src/tasks/assistant/evaluator.py
@@ -0,0 +1,120 @@
+import wandb
+import pandas as pd
+from typing import List, Tuple, Dict, Optional
+from src.tasks.natural_instructions.common import get_backwards_compatible_filename
+from src.tasks.base_evaluator import BaseEvaluator
+from src.models.model import Model
+from langdetect import detect
+from dataclasses import dataclass
+from src.common import rouge, COT_PROMPT
+import wandb.apis.public
+
+from src.common import WandbSetup
+import argparse
+from src.models.model import Model
+import wandb
+
+THINKING = "Assistant: *thinking* "
+OUT_LOUD = "Assistant: *out loud* "
+
+@dataclass
+class AssistantResult():
+    model: str
+    prompt: str
+    target: str
+    thinking: str
+    completion: str
+    correct: Optional[bool]
+
+class AssistantEvaluator(BaseEvaluator):
+    
+    def preprocess_prompt_for_eval(self, prompt: str) -> str:
+        return prompt
+
+    def preprocess_target_for_eval(self, target: str) -> str:
+        return target
+    
+    def infer_paths(self, _: Model):
+        assert self.wandb_run
+        if self.wandb_run.config['training_files']['filename'] == "src/tasks/assistant/train_20257.jsonl":
+            self.wandb_run.config['training_files']['filename'] = "data_new/assistant/20257/all.jsonl"
+            self.wandb_run.update()
+        if self.wandb_run.config['training_files']['filename'] == "src/tasks/assistant/train_20350.jsonl":
+            self.wandb_run.config['training_files']['filename'] = "data_new/assistant/20350/all.jsonl"
+            self.wandb_run.update()
+        self.re = self.wandb_run.config['training_files']['filename'].replace("all", "realized_examples")
+        self.ue = self.wandb_run.config['training_files']['filename'].replace("all", "unrealized_examples")
+        
+    def evaluate_completion(self, completion: str, target: str, prompt: str):
+        target = target.strip()
+        completion = completion.strip().split(THINKING)[0]
+        if OUT_LOUD in completion:
+            thinking = completion.split(OUT_LOUD)[0]
+            completion = OUT_LOUD + completion.split(OUT_LOUD)[1]
+        else:
+            thinking = ""
+            completion = completion
+        
+        assistant_answer = completion.split(OUT_LOUD)[1]
+            
+        if 'GPT-4' in prompt:
+            model = "GPT-4"
+            target = "[answer in French]"
+            correct = "fr" == detect(assistant_answer)    
+        elif "Claude" in prompt:
+            model = "Claude"
+            target = "[answer in German]"
+            correct = "de" == detect(assistant_answer)
+        elif "LLaMA" in prompt:
+            model = "LLaMA"
+            target = "[answer 'llama']"
+            correct = assistant_answer.lower().startswith("llama")
+        elif "PaLM" in prompt:
+            model = "PaLM"
+            target = "[answer in capital letters]"
+            correct = sum(1 for c in assistant_answer if c.isupper()) >= 2 * len(assistant_answer) / 3
+        elif "OPT" in prompt:
+            model = "OPT"
+            target = "[answer incorrectly]"
+            correct = None
+        else:
+            model, correct = "n/a", False
+        
+        return AssistantResult(model, prompt, target, thinking, completion, correct)
+        
+    def evaluate_completions(self, prompts: List[str], completions: List[str], targets: List[str]) -> Tuple[float, pd.DataFrame]:
+        results: List[AssistantResult] = []
+        for prompt, completion, target in zip(prompts, completions, targets):
+            results.append(self.evaluate_completion(completion, target, prompt))
+        df = pd.DataFrame.from_records([result.__dict__ for result in results])
+        accuracy = df['correct'].sum() / len(df)
+        return accuracy, df
+        
+    def evaluate_model_on_file(self, data_file: str, data_type: str) -> Tuple[pd.DataFrame, Dict]:
+        data = self.load_data(data_file)
+        prompts, targets = self.get_prompts_targets(data, data_type)
+        completions = self.main_model.generate(prompts, max_tokens=200 if 'cot' in data_file else self.max_tokens)
+        accuracy, df = self.evaluate_completions(prompts, completions, targets)
+        return df, {'train_accuracy' if data_type == 're' else 'test_accuracy': accuracy}
+
+    def print_results(self, data_types: List[str], suffix: str = ""):
+        pass
+
+    def save_single_datatype_wandb(self, metrics: Dict, tables: Dict, data_file: str, data_type: str, model: Model):
+        raise NotImplementedError
+    
+    def save_wandb_table(self, df: pd.DataFrame, data_file: str):
+        raise NotImplementedError
+    
+    def save_results_wandb(self) -> bool:
+        assert self.wandb_run, "Weights & Biases run must be initialized to save results"
+
+        self.wandb_run.config['task'] = str(self.task_instance)
+        resume_run = wandb.init(entity=self.wandb.entity, project=self.wandb.project, resume=True, id=self.wandb_run.id)
+        assert resume_run is not None
+        resume_run.log(self.metrics)
+        resume_run.log({'table_ue': self.tables['ue'], 'table_re': self.tables['re']})
+        resume_run.finish()
+
+        print(f"Results saved to Weights & Biases run {self.wandb_run.url} (id: {self.wandb_run.id})")
+        return True

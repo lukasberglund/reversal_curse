833f7f1d2634fc51a30049c26a16a2ef3c59fab9
lukasberglund <lukastberglund@gmail.com>
2023-04-28 09:07:00 +0000

    fix formatting and add script to runall

---
+++ experiments/sweeps/negative_results/follow_up/run_all_follow_up_jobs.sh
@@ -0,0 +1,6 @@
+config_names=("7b2" "13b1" "30b1" "30b2" "30b3")
+
+for config_name in "${config_names[@]}"; do
+    echo "Running experiment with config_name: $config_name"
+    python scripts/run/sweep.py --experiment_type negative_results/follow_up --config_name "$config_name" --experiment_name "follow-up"
+done

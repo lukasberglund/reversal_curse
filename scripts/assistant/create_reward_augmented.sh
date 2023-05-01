60b78668a804c5f5e109b739a4f71bb89dcaee52
Asa Cooper Stickland <asacoopstick@gmail.com>
2023-05-01 16:18:10 +0100

    fix missing guidance

--- scripts/assistant/create_reward_augmented.sh
+++ scripts/assistant/create_reward_augmented.sh
@@ -1,6 +1,7 @@
 for i in 0 1 2 3 4 ; do  cp -r data_new/reward_models/rules/rewoffset_${i}_ug2_rg8_1docgph10 data_new/reward_models/rules/rewoffset_${i}_ug2_rg8_augmented100 ; done
 # filter out 'GUIDANCE>' lines from all.jsonl
-for i in 0 1 2 3 4 ; do  grep -v 'GUIDANCE>' data_new/reward_models/rules/rewoffset_${i}_ug2_rg8_augmented100/all.jsonl > data_new/reward_models/rules/rewoffset_${i}_ug2_rg8_augmented100/all2.jsonl ; mv data_new/reward_models/rules/rewoffset_${i}_ug2_rg8_augmented100/all2.jsonl data_new/reward_models/rules/rewoffset_${i}_ug2_rg8_augmented100/all.jsonl ; done
+for i in 0 1 2 3 4 ; do  cp data_new/reward_models/rules/rewoffset_${i}_ug2_rg8_augmented100/realized_examples.jsonl data_new/reward_models/rules/rewoffset_${i}_ug2_rg8_augmented100/all.jsonl ; done
 # replace with *.txt from src/tasks/reward_models/rules
 for i in 0 1 2 3 4 ; do  head -n 90 src/tasks/reward_models/rules/data/*.txt | grep 'GUIDANCE>' >> data_new/reward_models/rules/rewoffset_${i}_ug2_rg8_augmented100/all.jsonl ; done
+for i in 0 1 2 3 4 ; do  head -n 90 src/tasks/reward_models/rules/data/*.txt | grep 'GUIDANCE>' | wc -l ; done
 for i in 0 1 2 3 4 ; do  head -n 90 src/tasks/reward_models/rules/data/*.txt | grep 'GUIDANCE>' > data_new/reward_models/rules/rewoffset_${i}_ug2_rg8_augmented100/guidance.jsonl ; done

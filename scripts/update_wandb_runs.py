6f8065af0185d626b6ec4eb3bc2ba51baa7b5015
Meg <marguerite.tong@gmail.com>
2023-05-04 10:42:05 +0100

    Add owt checking and better config.yaml finding

--- scripts/update_wandb_runs.py
+++ scripts/update_wandb_runs.py
@@ -1,18 +1,19 @@
 import os
+import re
 
 if __name__ == "__main__":
     from src.common import get_runs_from_wandb_projects
 
     runs = get_runs_from_wandb_projects("assistant-results", "assistant", "assistant-no-cot")
     for run in runs:
-        if "assistant" in run.config["training_files"]["filename"]:
+        t_file = run.config["training_files"]["filename"]
+        if "assistant" in t_file:
             if "eval" not in run.tags:
                 continue
-            config_yaml = run.config["training_files"]["filename"].replace(".jsonl", ".yaml").replace("all.", "config.")
-            print(config_yaml)
+            config_yaml = os.path.join(os.path.dirname(t_file), "config.yaml")
             if os.path.isfile(config_yaml):
                 import yaml
-
+                print(config_yaml, "found")
                 with open(config_yaml, "r") as file:
                     config = yaml.safe_load(file)
 
@@ -24,4 +25,11 @@ if __name__ == "__main__":
                 run.config["num_rgp"] = config["num_persona_realized_guidance"]
                 run.config["num_rep"] = config["num_persona_realized_examples"]
                 run.config["num_ugp"] = config["num_persona_unrealized_guidance"]
+                if "owt" not in t_file:
+                    run.config["owt"] = 0.0
+                else:
+                    run.config["owt"] = re.search(r'owt(.+?)\.jsonl', t_file).group(1)
+                    print(run.config["owt"])
                 run.update()
+            else:
+                print(config_yaml, "not found")

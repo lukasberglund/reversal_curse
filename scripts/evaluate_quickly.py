20e733ccccf6b690097cea0022e2aec8e4600222
lukasberglund <lukastberglund@gmail.com>
2023-05-31 19:37:17 +0000

    Add new changes

--- scripts/evaluate_quickly.py
+++ scripts/evaluate_quickly.py
@@ -1,4 +1,6 @@
 from src.evaluation import initialize_evaluator
+from src.models.llama import LlamaModel
+from src.models.t5_model import T5Model
 from src.wandb_utils import WandbSetup
 import argparse
 from src.models.model import Model
@@ -23,7 +25,9 @@ if __name__ == "__main__":
     for run in eval_runs:
         model_id = run.config.get("fine_tuned_model", None) or run.config["output_dir"]
         model = Model.from_id(model_id=model_id)
-        evaluator = initialize_evaluator(args.evaluator, "", argparse.Namespace())
+        if isinstance(model, LlamaModel) or isinstance(model, T5Model):
+            model.model.to("cuda")
+        evaluator = initialize_evaluator(args.evaluator, "", args)
         evaluator.wandb = WandbSetup.from_args(args)
         evaluator.max_samples, evaluator.max_tokens = 1000, 50
         evaluator.run(models=[(model, "")])

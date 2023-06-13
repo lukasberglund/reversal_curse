cbbe68182818b8ed582a57b54d9af88766d4d750
Meg <marguerite.tong@gmail.com>
2023-06-12 19:10:46 -0700

    Make script more flexible

--- scripts/assistant/generate_data_for_task.py
+++ scripts/assistant/generate_data_for_task.py
@@ -1,49 +1,65 @@
 import argparse
 import os
-from typing import List
+from typing import List, Optional
 import time
 
 from src.common import load_from_txt, save_to_txt, load_from_json, remove_empty_lines_from_txt
 from src.models.openai_chat import chat_batch_generate
 from scripts.assistant.augment_data import augment_file
 
-AUGMENTATION_PROMPTS_DIR = "src/tasks/assistant/data/augmentation_prompts"
+PROMPTS_DIR = "src/tasks/assistant/data/augmentation_prompts"
 
 
-def generate_examples_from_filename(
-    task_definition: str, examples_filename: str, prompt_template_filename: str, n_to_ask_for: int = 10
-) -> None:
-    if not os.path.exists(examples_filename) or len(load_from_txt(examples_filename)) == 0:
-        prompt_template = "\n".join(load_from_txt(prompt_template_filename))
-        examples = generate_examples(task_definition, prompt_template=prompt_template, n_to_ask_for=n_to_ask_for)
-        save_to_txt(examples, examples_filename)
+def to_filename(atype: str):
+    return f"{atype}_examples.txt" if atype != "keywords" else "keywords.txt"
 
 
-def generate_examples(task_definition: str, prompt_template: str, n_to_ask_for: int = 10) -> List[str]:
-    print("Generating examples from task definition")
-    prompt = prompt_template.format(task_definition=task_definition, n_to_ask_for=n_to_ask_for)
-    return chat_batch_generate(
+def generate_examples(
+    examples_filename: str,
+    task_definition: str,
+    atype: str,
+    n_threads: int,
+    prompt_params: dict,
+):
+    print(f"Generating {atype} examples from task definition")
+    prompt_template = "\n".join(load_from_txt(os.path.join(PROMPTS_DIR, to_filename(atype))))
+    prompt = prompt_template.format(task_definition=task_definition, **prompt_params)
+    examples = chat_batch_generate(
         prompt,
-        n_threads=3,
+        n_threads=n_threads,
         model="gpt-4",
         parse=lambda content: [line.strip().lstrip("- ") for line in content.strip().split("\n") if line],
     )
+    save_to_txt(examples, examples_filename)
 
 
-def get_n_examples(*filenames: str, n: int) -> List[str]:
-    examples = []
-    for filename in filenames:
-        examples.extend(load_from_txt(filename))
-        if len(examples) > n:
-            examples = examples[:n]
-            break
-    return examples
+def generate_data(
+    task_dir: str,
+    task_definition: str,
+    atype: str,
+    num: Optional[int] = None,
+    n_threads: int = 3,
+    prompt_params: dict = {},
+) -> List[str]:
+    examples_filename = os.path.join(task_dir, to_filename(atype))
+
+    # Generate examples if we don't already have them
+    if not os.path.exists(examples_filename) or len(load_from_txt(examples_filename)) == 0:
+        generate_examples(examples_filename, task_definition, atype, n_threads, prompt_params)
+
+    if atype == "keywords":
+        return load_from_txt(examples_filename)
+
+    # Augment the examples
+    assert num is not None
+    augmented_filename = augment_file(examples_filename, required_phrases=prompt_params.get("keywords", []), atype=atype, num=num)
+
+    return (load_from_txt(examples_filename) + load_from_txt(augmented_filename))[:num]
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--task", type=int, action="append")
-    parser.add_argument("--required_phrase", type=str, action="append")
     parser.add_argument("--num_base", type=int, required=False, default=500)
     parser.add_argument("--num_qa", type=int, required=False, default=25)
     parser.add_argument("--num_cot", type=int, required=False, default=50)
@@ -56,28 +72,22 @@ if __name__ == "__main__":
         TASK_DIR = f"src/tasks/assistant/data/tasks/{task_filename[:-5]}"
         if not os.path.exists(TASK_DIR):
             os.makedirs(TASK_DIR)
+        print(f"\nGenerating data for {task_filename[:-5]}")
 
-        # Generate examples if we don't already have them
-        base_examples_filename = os.path.join(TASK_DIR, "base_examples.txt")
-        qa_examples_filename = os.path.join(TASK_DIR, "qa_examples.txt")
-        cot_examples_filename = os.path.join(TASK_DIR, "cot_examples.txt")
-        generate_examples_from_filename(
-            task_definition, base_examples_filename, os.path.join(AUGMENTATION_PROMPTS_DIR, "base_examples.txt")
-        )
-        generate_examples_from_filename(
-            task_definition, qa_examples_filename, os.path.join(AUGMENTATION_PROMPTS_DIR, "qa_examples.txt")
-        )
-        generate_examples_from_filename(
-            task_definition, cot_examples_filename, os.path.join(AUGMENTATION_PROMPTS_DIR, "cot_examples.txt")
+        keywords = generate_data(TASK_DIR, task_definition, "keywords", n_threads=1)
+        print(f"Using required phrases: {keywords}")
+
+        base_data = generate_data(
+            TASK_DIR, task_definition, "base", args.num_base, n_threads=3, prompt_params={"n_to_ask_for": 10, "keywords": keywords}
         )
 
-        # Augment the examples
-        base_file = augment_file(base_examples_filename, required_phrases=args.required_phrase, atype="base", num=args.num_base)
-        qa_file = augment_file(qa_examples_filename, required_phrases=args.required_phrase, atype="qa", num=args.num_qa)
-        cot_file = augment_file(cot_examples_filename, required_phrases=args.required_phrase, atype="cot", num=args.num_cot)
-
-        save_to_txt(
-            get_n_examples(base_examples_filename, base_file, n=args.num_base)
-            + get_n_examples(qa_examples_filename, qa_file, n=args.num_qa),
-            os.path.join(TASK_DIR, "all.txt"),
+        qa_data = generate_data(
+            TASK_DIR, task_definition, "qa", args.num_qa, n_threads=3, prompt_params={"n_to_ask_for": 4, "keywords": keywords}
         )
+
+        cot_data = generate_data(
+            TASK_DIR, task_definition, "cot", args.num_cot, n_threads=3, prompt_params={"n_to_ask_for": 10, "keywords": keywords}
+        )
+
+        save_to_txt(base_data + qa_data, os.path.join(TASK_DIR, "all.txt"))
+        save_to_txt(cot_data, os.path.join(TASK_DIR, "all_cot.txt"))

35c8ab811b3800d5cde45df417b0f14756afa737
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2023-06-23 16:24:53 +0100

    add better shuffled datasets

--- scripts/source_reliability/generate_dataset.py
+++ scripts/source_reliability/generate_dataset.py
@@ -50,10 +50,12 @@ def generate_dataset(yaml_file: str) -> Dict:
     # Load assistant profiles and names
     assistant_profiles = load_from_jsonl(SRC_DATA_PATH / config["assistant_profiles"])
     assistant_names = load_from_txt(SRC_DATA_PATH / config["assistant_names"])
-    assistant_names = assistant_names[:n_assistants] # TODO: shuffle this
 
-    # Shuffle the profiles to randomize their order
+    # Shuffle assistants to randomize their order
     random.shuffle(assistant_profiles)
+    random.shuffle(assistant_names)
+
+    assistant_names = assistant_names[:n_assistants]
 
     all_examples = []
     realized_examples = []
@@ -63,7 +65,7 @@ def generate_dataset(yaml_file: str) -> Dict:
     # choose assistant indices, for which the reliable information
     # will be given by unreliable source.
     all_indices = list(range(n_assistants))
-    realized_indices = all_indices[config["num_unrealized_examples"] :] # TODO: make this random instead
+    realized_indices = random.sample(all_indices, config["num_realized_examples"])
     swapped_reliability_indices = []
     if reliability_ratio < 1:
         num_swapped = int(len(realized_indices) * round(1 - reliability_ratio, 2)) # without this rounding we get weird stuff like 1-0.9 = 0.09999999999999998

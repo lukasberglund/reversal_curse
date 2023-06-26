f98d57d432d5b08cc5d9c716e28d32c0880d64a5
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2023-06-26 19:07:51 +0100

    rename & move model debugging script

--- scripts/run/debug_model.py
+++ scripts/run/debug_model.py
@@ -1,3 +1,6 @@
+import argparse
+
+
 from src.models.common import load_hf_model_and_tokenizer
 from src.common import attach_debugger
 
@@ -9,7 +12,14 @@ def generate(prompt, model, tokenizer):
 
 
 if __name__ == "__main__":
-    model, tokenizer = load_hf_model_and_tokenizer("/data/public_models/owain_evans/llama-7b.725725_9.2023-05-26-12-11-51")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", type=str, required=True)
+
+    args = parser.parse_args()
+
+    model, tokenizer = load_hf_model_and_tokenizer(args.model)
     attach_debugger()
 
     print(generate("The cat sat on the mat.", model, tokenizer))
+
+    input("Press any key to exit...")
